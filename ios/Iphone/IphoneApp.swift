2313f75d66303f7d2023389b01650008c37e8d4d
mitchbr <mitchell.dee.brown@gmail.com>
2022-11-06 08:42:24 -0800

    set up ios builds

---
+++ ios/Iphone/IphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  IphoneApp.swift
+//  Iphone
+//
+//  Created by Mitchell Brown on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct IphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
